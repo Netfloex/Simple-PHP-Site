2b88aeddf45e2fa76040fa6b2edcb86c31b9f97e
Sam Taen <netfloex@gmail.com>
2020-10-23 23:21:24 +0000

    automatically create app.css if not exists

--- bin/styling.php
+++ bin/styling.php
@@ -1,8 +1,12 @@
 <?php
 require "lessc.inc.php";
 $less = new lessc;
+if (!file_exists($paths->stylesOutput)) {
+    mkdir(preg_replace(["/[^\/]+$/",], "", $paths->stylesOutput), 0777, true);
+    touch($paths->stylesOutput, 0);
+}
 try {
-    $less->checkedCompile(jPath($paths->stylesInput), jPath($paths->stylesOutput));
+    $less->checkedCompile(jPath($paths->stylesInput), $paths->stylesOutput);
 } catch (\Throwable $th) {
     echo "Error during less > css conversion" . $th;
 }
--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="<?= preg_replace("/^{$paths->public}/", "", $paths->stylesOutput) ?>">
+    <link rel="stylesheet" href="<?= $paths->stylesOutput ?>">
 </head>
 
 <body>
